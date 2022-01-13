9c59c1534565d5bb16f62184a39cc5c7888bb80e
ifte99 <87614310+ifte99@users.noreply.github.com>
2022-01-14 02:08:02 +0600

    !st update
    3 structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DragComponent } from './drag/drag.component';
+import { SelectComponent } from './select/select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragComponent,
+    SelectComponent
   ],
   imports: [
     BrowserModule,
